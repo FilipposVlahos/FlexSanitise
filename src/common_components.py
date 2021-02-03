c1c24dd79665bdf1c5926557eb236d64d1fa0bfa
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-02-03 18:32:01 +0000

    Delete common_components

--- src/common_components.py
+++
@@ -1,9 +0,0 @@
-class DocumentRetrieval:
-    fileLocation: str
-
-    def __init__(self):
-        self.fileLocation = "data_set/Lease.txt"
-
-    def get_document(self):
-        file = open(self.fileLocation, "r")
-        return file.read()
