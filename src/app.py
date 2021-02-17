db76acef3f8695bebc4c17d50372aa22db17ec18
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-02-17 02:26:29 +0000

    Multi-threading

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_cors import CORS
 import json
 import os
+import multiprocessing as mp
+from multiprocessing import Process, Queue
 
 from allennlp_answerer import PythonPredictor
 from regex_sanitizations import RegexSanitization
@@ -37,22 +39,34 @@ def sanitise_document_regex(document, regex):
         result = regex_sanitiser.sanitise_email_addresses(result)
     return result
 
-def sanitise_document_qa(document, questions):
-    answers = []
-    questions = json.loads(questions)
+questQueue = Queue()
+answerQueue = Queue()
 
+def worker(document):
+    while not questQueue.empty():
+        question = questQueue.get()
+        print("Question ", "question")
+        answerQueue.put(answerer.predict(document, question))
+
+def sanitise_document_qa(document, questions):
+    questions = json.loads(questions)
     for question in questions["questions"]:
-        print("Question", ": ", question)
-        # find answer to question
-        answer = answerer.predict(document, question)
-        print("Answer ", ": ", answer["answer"])
-        print()
-        answers.append(answer["answer"])
-    
+        questQueue.put(question)
+
+    nu_of_cpus = mp.cpu_count()
+    p = []
+    for i in range(nu_of_cpus):
+        process = Process(target=worker, args=(document,))
+        p.append(process)
+        process.start()
+
+    for process in p: 
+        process.join()
+
     # replace
     sanitised_text = document
-    for answer in answers:
-        sanitised_text = sanitised_text.replace(answer, "[Sensitive Information]")
+    while not answerQueue.empty():
+        sanitised_text = sanitised_text.replace(answerQueue.get(), "[Sensitive Information]")
 
     return sanitised_text
 
