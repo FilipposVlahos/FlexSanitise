f198c43a48efe205cb4d49c562cb102b564b991f
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-02-17 02:27:22 +0000

    Added option to use transformer model
    Also now returns string instesd of dict

--- src/allennlp_answerer.py
+++ src/allennlp_answerer.py
@@ -5,10 +5,12 @@ class PythonPredictor:
     def __init__(self):
         self.predictor = Predictor.from_path(
             "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz")
+        # self.predictor = Predictor.from_path(
+        #     "https://storage.googleapis.com/allennlp-public-models/transformer-qa-2020-10-03.tar.gz")
 
     def predict(self, passage, question):
         prediction = self.predictor.predict(
             passage=passage, question=question
         )
 
-        return {'answer': prediction["best_span_str"]}
+        return prediction["best_span_str"]
