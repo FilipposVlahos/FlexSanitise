01f9145ee09532f1b8c20db9d0b1d2b8ba5e47f9
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-03-20 16:45:00 +0000

    Changes to app.py
    * added Named Entity Recognition API listening on /ner
    * added comments to functions

--- src/app.py
+++ src/app.py
@@ -4,23 +4,46 @@ import os
 
 from regex_sanitisations import RegexSanitisation
 from qa_sanitiser import QASanitiser
+from ner import NamedEntityRecogniser
 
 app = Flask(__name__)
 cors = CORS(app)
 
 regex_sanitiser = RegexSanitisation()
 qa_sanitiser = QASanitiser()
+named_entity_recogniser = NamedEntityRecogniser()
 
 @app.route('/document', methods=['POST'])
 def sanitise():
-    if not request.json or not "document" or not "questions" or not "regex" in request.json:
+    '''
+        Takes a document and returns a sanitised document and a highlighted document
+            document: The document to be sanitised
+            questions: List of questions the answers of which will be reducted from the document
+            regex: List containing types of structured data to be sanitised. For more info look in regex_sanitisations.py
+            ner: List of lists containing of the form [named_entity, label]. Wherever named_entity appears in document replace it with label
+    '''
+    if not request.json or not "document" or not "questions" or not "regex" or not "ner" in request.json:
         abort(400)
-    document, questions, regex = request.json["document"], request.json["questions"], request.json["regex"]
+    document, questions, regex, ner = request.json["document"], request.json["questions"], request.json["regex"], request.json["ner"]
     print("Document:", document, "\n" "Questions:", questions, "\n", "Regex:", regex)
-    docs = {"docToSanitise": document, "docToHighlight": document}
+    docs = {"sanitisedDocument": document, "highlightedDocument": document}
     docs = qa_sanitiser.sanitise_qa(docs, questions)
     docs = regex_sanitiser.sanitise_document_regex(docs, regex)
+    docs = named_entity_recogniser.sanitiseNer(docs, ner)
     return jsonify(docs)
+
+@app.route('/ner', methods=['POST'])
+def ner():
+    '''
+        Takes a document and returns list of lists of the form [named_entity, label]
+            document: Document from which named entities will be recognised
+    '''
+    if not request.json or not "document" in request.json:
+        abort(400)
+    document = request.json["document"]
+    name_entities = named_entity_recogniser.getNamedEntities(document)
+    return (jsonify(name_entities))
+
 
 if __name__ == '__main__':
     app.run()
