ae31a17542a1776bef418ce57a5e3ef0d03a155b
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-03-20 16:45:51 +0000

    ner.py
    class for Named Entity Recognition

---
+++ src/ner.py
@@ -0,0 +1,26 @@
+import spacy
+import en_core_web_trf
+
+class NamedEntityRecogniser:
+    def __init__(self):
+        self.nlp = en_core_web_trf.load()
+
+    def getNamedEntities(self, document):
+        doc = self.nlp(document)
+        name_entities = []
+        uniqueEntities = set()
+        for X in doc.ents:
+            if X.text not in uniqueEntities:
+                name_entities.append([X.text, X.label_])
+                uniqueEntities.add(X.text)
+        return ({
+            "ner": name_entities
+        })
+
+    def sanitiseNer(self, documents, ner):
+        document_without_ne = documents["sanitisedDocument"]
+        highlighted_document = documents["highlightedDocument"]
+        for named_entity, label in ner:
+            document_without_ne = document_without_ne.replace(named_entity, "[" + label + "]")
+            highlighted_document = highlighted_document.replace(named_entity,  "<span style=\"background-color: yellow\">" + named_entity + "</span>")
+        return ({"sanitisedDocument": document_without_ne, "highlightedDocument": highlighted_document})
