6938f650154f675ac4cae7b9ce5ee13076624f07
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-04-09 02:49:24 +0100

    Highlight sanitised document

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_cors import CORS
-import os
+import re
 
 from regex_sanitisations import RegexSanitisation
 from qa_sanitiser import QASanitiser
@@ -18,7 +18,7 @@ def sanitise():
     '''
         Takes a document and returns a sanitised document and a highlighted document
             document: The document to be sanitised
-            questions: List of questions the answers of which will be reducted from the document
+            questions: List of questions the answers of which will be sanitised from the document
             regex: List containing types of structured data to be sanitised. For more info look in regex_sanitisations.py
             ner: List of lists containing of the form [named_entity, label]. Wherever named_entity appears in document replace it with label
     '''
@@ -30,6 +30,7 @@ def sanitise():
     docs = qa_sanitiser.sanitise_qa(docs, questions)
     docs = regex_sanitiser.sanitise_document_regex(docs, regex)
     docs = named_entity_recogniser.sanitiseNer(docs, ner)
+    docs = highlight_sanitised_document(docs)
     return jsonify(docs)
 
 @app.route('/ner', methods=['POST'])
@@ -44,6 +45,15 @@ def ner():
     name_entities = named_entity_recogniser.getNamedEntities(document)
     return (jsonify(name_entities))
 
+def highlight_sanitised_document(docs):
+    '''
+        Highlights text in brackets within the sanitisedDocument 
+    '''
+    sanitised_doc = docs["sanitisedDocument"]
+    sanitisedDocument = re.sub(
+        r'(\[.*?\])', r'<span style="background-color: yellow">\1</span>', sanitised_doc)
+    docs["sanitisedDocument"] = sanitisedDocument
+    return docs
 
 if __name__ == '__main__':
     app.run()
